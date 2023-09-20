3143a53b41f022ad36088413985ed267218d37d9
Fred Suter <suterf@ornl.gov>
2023-09-19 21:00:49 -0400

    first cleanup pass on platform creation

--- main.cpp
+++ main.cpp
@@ -37,7 +37,7 @@ public:
             todo.push_front(new WorkUnit(1, 10*MB, 2*GFLOP));
         }
         // Add "poison pills" at the end, one per worker
-        for (int i=0; i < num_workers; i++) {
+        for (unsigned int i=0; i < num_workers; i++) {
             todo.push_front(new WorkUnit(0,0,0));
         }
 
@@ -58,7 +58,7 @@ public:
     void operator()() {
         auto cluster_hosts = sg4::this_actor::get_host()->get_englobing_zone()->get_all_hosts();
         auto db_host = sg4::Host::by_name("database.org");
-        auto db_disk = db_host->get_disks().at(0);
+        auto db_disk = db_host->get_disks().front();
         auto my_mailbox = sg4::Mailbox::by_name("mb_" + sg4::this_actor::get_name());
 
         XBT_INFO("Worker starting on a cluster with %ld compute nodes.", cluster_hosts.size());
@@ -117,7 +117,7 @@ public:
 };
 
 
-static std::tuple<sg4::NetZone*, simgrid::kernel::routing::NetPoint*> create_cluster(const sg4::NetZone* root,
+static std::pair<sg4::NetZone*, simgrid::kernel::routing::NetPoint*> create_cluster(const sg4::NetZone* root,
                                                                                      const std::string& cluster_suffix,
                                                                                      const int num_hosts)
 {
@@ -125,69 +125,62 @@ static std::tuple<sg4::NetZone*, simgrid::kernel::routing::NetPoint*> create_clu
     cluster->set_parent(root);
 
     /* create the backbone link */
-    const sg4::Link* l_bb = cluster->create_link("backbone" + cluster_suffix, 100*GB)->set_latency(1e-4)->seal();
+    sg4::LinkInRoute backbone{cluster->create_link("backbone" + cluster_suffix, "100Gbps")->set_latency("100us")};
 
     /* create all hosts and connect them to outside world */
     for (int i=0; i < num_hosts; i++) {
         std::string hostname = "host-" + std::to_string(i)  + cluster_suffix;
         /* create host */
-        const sg4::Host* host = cluster->create_host(hostname, 1*GFLOP);
-        /* create UP link */
-        const sg4::Link* l_up = cluster->create_link(hostname + "_up", 10*GB)->set_latency(1e-5)->seal();
-        /* create DOWN link, if needed */
-        const sg4::Link* l_down = l_up;
-        if (i != 0) {
-            l_down = cluster->create_link(hostname + "_down", 10*GB)->set_latency(1e-5)->seal();
-        }
-        sg4::LinkInRoute backbone{l_bb};
-        sg4::LinkInRoute link_up{l_up};
-        sg4::LinkInRoute link_down{l_down};
-
-        /* add link UP and backbone for communications from the host */
-        cluster->add_route(host->get_netpoint(), nullptr, nullptr, nullptr, {link_up, backbone}, false);
-        /* add backbone and link DOWN for communications to the host */
-        cluster->add_route(nullptr, host->get_netpoint(), nullptr, nullptr, {backbone, link_down}, false);
+        const auto* host = cluster->create_host(hostname, "1Gf");
+        /* create link */
+        sg4::LinkInRoute link{cluster->create_link(hostname + "_link", "10Gbps")->set_latency("10us")};
+        /* add route between host and any other host */
+        cluster->add_route(host->get_netpoint(), nullptr, nullptr, nullptr, {link, backbone});
     }
 
     /* create router */
     auto router = cluster->create_router("router" + cluster_suffix);
 
     cluster->seal();
-    return std::make_tuple(cluster, router);
+    return std::make_pair(cluster, router);
 }
 
 int main(int argc, char **argv) {
     auto engine = new sg4::Engine(&argc, argv);
 
     // Create the platform
-    auto* root = sg4::create_full_zone("AS0");
+    auto* root = sg4::create_full_zone("world");
 
     // Create a coordinator zone/host
     auto coordinator_zone = sg4::create_full_zone("Coordinator")->set_parent(root);
-    auto coordinator_host = coordinator_zone->create_host("coordinator.org", 1*GFLOP);
+    auto coordinator_host = coordinator_zone->create_host("coordinator.org", "1Gf");
 
-    // Create a database zone/host 
+    // Create a database zone/host
     auto database_zone = sg4::create_full_zone("Database")->set_parent(root);
-    auto database_host = database_zone->create_host("database.org", 1*GFLOP);
-    database_host->create_disk("db", 100*MB, 50*MB);
+    auto database_host = database_zone->create_host("database.org", "1Gf");
+    database_host->create_disk("db", "100MBps", "50MBps");
 
     // Create a single link as a simple abstraction of the whole wide-area network
-    const sg4::Link* l = root->create_link("link1-2", 200*MB)->set_latency(1e-3)->seal();
-    sg4::LinkInRoute internet{l};
+    sg4::LinkInRoute internet{root->create_link("internet", "200MBps")->set_latency("1ms")};
 
     // Create three clusters
     std::vector<int> cluster_sizes = {16, 32, 40};
-    for (int i=0; i < cluster_sizes.size(); i++) {
-        auto cluster = create_cluster(root, ".cluster" + std::to_string(i) + ".org", cluster_sizes.at(i));
-        root->add_route(coordinator_zone->get_netpoint(), std::get<0>(cluster)->get_netpoint(), coordinator_host->get_netpoint(), std::get<1>(cluster), {internet});
-        root->add_route(database_zone->get_netpoint(), std::get<0>(cluster)->get_netpoint(), database_host->get_netpoint(), std::get<1>(cluster), {internet});
+    int i = 0;
+    sg4::NetZone* cluster;
+    simgrid::kernel::routing::NetPoint* router;
+    for (auto size: cluster_sizes) {
+        std::tie(cluster, router) = create_cluster(root, ".cluster" + std::to_string(i++) + ".org", size);
+        root->add_route(coordinator_zone->get_netpoint(), cluster->get_netpoint(),
+                        coordinator_host->get_netpoint(), router, {internet});
+        root->add_route(database_zone->get_netpoint(), cluster->get_netpoint(),
+                        database_host->get_netpoint(), router, {internet});
     }
 
     root->seal();
 
     // Create the coordinator and worker actors
     sg4::Actor::create("Coordinator", sg4::Host::by_name("coordinator.org"), Coordinator());
-    for (int i=0; i < cluster_sizes.size(); i++) {
+    for (unsigned int i = 0; i < cluster_sizes.size(); i++) {
         sg4::Actor::create("Worker" + std::to_string(i), sg4::Host::by_name("host-0.cluster" + std::to_string(i) + ".org"), Worker());
     }
 
